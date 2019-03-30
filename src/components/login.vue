f3c70fabcc50cea55df37d02a906a065afbd8cf9
QAQchengyu <1210670609@qq.com>
2019-03-30 13:14:31 +0800

    登录页面静态完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>登录页</div>
+  <div class="login">
+    <div class="box">
+      <h2>登录页面</h2>
+      <el-form label-position="top" label-width="80px">
+        <el-form-item label="用户名">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="button">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +23,22 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.login {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box {
+    width: 550px;
+    height: 420px;
+    background-color: #fff;
+    padding: 35px;
+    border-radius: 15px;
+    .button {
+      width: 100%;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入elmUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 导入公共样式
+import './assets/base.scss'
+
 Vue.config.productionTip = false
 
 // 导入路由
